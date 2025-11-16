c02a3b86fb8ee5126023eabe145b0ced3c10e045
Nguyen1976 <nguyen2202794@gmail.com>
2025-11-16 11:28:03 +0700

    feat: initialize prisma-scheme with initial configuration and migrations
    - Added package.json with dependencies and devDependencies for Prisma and TypeScript.
    - Created prisma.config.ts for Prisma configuration.
    - Added initial migration files for creating tables xxxx_01 and xxxx_02.
    - Included migration file for dropping table xxxx_02.
    - Created migration lock file for version control.
    - Defined Prisma schema with models for xxxx, Yyyy, User, Category, PostCategory, Post, and Profile.
    - Added a readme.md file for project documentation.

---
+++ prisma-scheme/prisma.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'prisma/config'
+
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+  },
+  engine: 'classic',
+  datasource: {
+    url: 'mysql://root:123456@localhost:3306/learn_prisma_01',
+  },
+})
---
+++ prisma-scheme/prisma/migrations/20251116041940_undo_update_post/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the `xxxx_02` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `xxxx_02`;
